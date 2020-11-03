9b638f8c859087ec0371c0a6a4119e9f43eec97f
maybeb50 <public.maybe.b50@gmail.com>
2020-11-03 14:28:32 +0900

    http -> https 변경 테스트

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -14,7 +14,6 @@ const TodoTemplateBlock = styled.div`
 `;
 
 function TodoTemplate({ children }) {
-    console.log({children});
     return (
         <TodoTemplateBlock>
             { children }
